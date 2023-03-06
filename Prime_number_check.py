3c532b95b32fbfe3c242003efb54bcb907e43d13
Saad's grimoire of sorceries <114657050+Applethal@users.noreply.github.com>
2023-03-06 22:12:59 +0100

    Update Prime_number_check.py

--- Prime_number_check.py
+++ Prime_number_check.py
@@ -1,27 +1,42 @@
 n = int(input("Enter the number to be checked."))
 
 
+def Prime_check(n):
 
-check = n 
-list = []
-while check > 0:
-    prime_test = n / check
-    if n % check == 0:
-        list.append(prime_test)
+    check = n
+    list = []
+    Prime = True
+    while check > 0:
+        prime_test = n / check
+        if n % check == 0:
+            list.append(prime_test)
+        else:
+            pass
+        check -= 1
+
+    if len(list) == 2:
+        #print("Yes, the number is a prime number")
+        Prime = True
+
     else:
-        pass
-    check -= 1
+        #print("The number is not a prime number")
+        Prime = False
 
-if len(list) == 2:
-    print("Yes, the number is a prime number")
-else:
-    print("The number is not a prime number")
+    return Prime
 
 
-## what is the n'th prime number 
-    
-prime_numbers = []
+N = int(input("What prime number are you looking for ?"))
 
-N = int(input("Input the position of the prime that you wish to know ?"))
 
-while prime_numbers[-1] == N:
+def Prime_n(N):
+    counter = 0
+    num = 1
+    while counter < N:
+        num += 1
+        if Prime_check(num):
+            counter += 1
+
+    print("The requested number is ", num)
+
+
+Prime_n(N)
