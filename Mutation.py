b1878ff444b40d78c7d488904df368deee4e42e9
Saad's grimoire of sorceries <114657050+Applethal@users.noreply.github.com>
2023-03-27 21:19:34 +0200

    Update Mutation.py

--- Mutation.py
+++ Mutation.py
@@ -1,27 +1,71 @@
+Parents = {}
 
-def Set():
-    limit = int(input("How many elements should your Set contain ?"))
-    Elements = []
+p = int(input("How many parents do you wish to have ?"))
+counter = 0
+
+for i in range(p):
+    Parents[i] = []
+    counter += 1
+    print("You are now processing parent number", counter)
+    limit = int(input("How many elements should this parent have"))
     while limit > 0:
-        Elements.append(int(input("Insert the element")))
+        Parents[i].append(int(input("Insert the element")))
         limit -= 1
-    print(Elements)
-    
-    return Elements
-Elements = Set()
+
+print(Parents)
 
 
+def Pick(Parents):
+    Choose = int(
+        input(
+            "From The Parents dictionary that you have created, which parent do you want to choose for the crossover"
+        )
+    )
+    parent1 = Parents.get(Choose)
+    Choose_new = int(input("Now choose the second parent"))
+    parent2 = Parents.get(Choose_new)
+    return parent1, parent2
 
 
+parent1, parent2 = Pick(Parents)
 
-cell = int(input("Input the new element that you wish to replace"))
-position = int(input("At which position should the element be inserted"))
+print("First Parent", parent1)
+print("Second Parent", parent2)
 
-def Mutation(Elements,position,cell):
-    
-    Elements.insert(position,cell)
-    Elements.pop(position-1)
-    print(Elements)
-    
 
-Mutation(Elements,position,cell)
+def Crossover(parent1, parent2):
+    if len(parent1) != len(parent2):
+        print(
+            "Unless I missed something, I don't see potential for a crossover with these parents, you might want to choose ones which have the same number of elements within their sets"
+        )
+    k = int(input("how many cuts do you wish to perform within the set ?"))
+    A = 0
+    child1 = []
+    child2 = []
+
+    for i in range(k):
+        while k > 1:
+            position = int(input("At which position should this cut occur"))
+            child1.extend(parent1[A:position])
+            child2.extend(parent2[A:position])
+            parent1, parent2 = parent2, parent1
+            A = position
+            print("first child", child1)
+            print("second child", child2)
+            k -= 1
+    if k == 1:
+        position = int(input("At which position should the last cut occur ?"))
+        child1.extend(parent1[A:position])
+        child2.extend(parent2[A:position])
+        parent1, parent2 = parent2, parent1
+        A = position
+        child1.extend(parent1[position:])
+        child2.extend(parent2[position:])
+
+    return child1, child2
+
+
+child1, child2 = Crossover(parent1, parent2)
+
+print("first child", child1)
+print("second child", child2)
