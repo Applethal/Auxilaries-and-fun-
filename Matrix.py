b58f387292c2313f6e75bec243803ffe5764853f
Saad's grimoire of sorceries <114657050+Applethal@users.noreply.github.com>
2023-04-24 12:03:21 +0200

    Update Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -26,32 +26,50 @@ class matrix:
                 number = int(input(f"Input the number in row {counter_r}, column  {counter_c}:"))
                 row.append(number)
 
+    def display_row(self):
+        k = int(input("Choose which row to in the matrix that you wish to display"))
+        k -= 1
+        row = self.matrix[k]
+        print(row)
+    def display_column(self):
+        m = int(input("Choose which column you wish to display in your matrix"))
+        column = []
+        m -= 1
+        for  i in range(len(self.matrix)):
+            number = self.matrix[i][m]
+            column.append(number)
+        print(column)
+    
+    # supose I want to multiply the same matrix by itself just for the sake of exercising
+    
     def multiplication(self):
-        self.add_row()
-        self.add_column()
-        if len(self.matrix[1]) != self.add_column:
-            print(
-                "Sorry, I don't think that's how multiplying a matrix works, the lenght of the row should match the lenght of the column of the array that you wish to multiply the matrix with."
-            )
-        else 
-            m_rows = self.add_row()
-            m_columns = self.add_column()
-            m_matrix = []
-            m_counter_c = 0
-            m_counter_r = 0
-            for i in range(m_rows):
-                i = []
-                m_matrix.append(i)
-                m_counter_r += 1
-                for j in range(m_columns):
-                    if m_counter_c == m_columns:
-                        m_counter_c = 0
-                    m_counter_c += 1
-                    n = int(input(f"Input the number in row {m_counter_r}, column  {m_counter_c}:"))
-                    i.append(n)
-            for i in self.matrix:
-                for j in self.matrix[i]:
-                    self.matrix[i][j] = self.matrix[j] * m_matrix[i][j]
+        matrix_m = []
+        for j in range(len(self.matrix[0])):
+            columns = []
+            for i in range(len(self.matrix)):
+                number = self.matrix[i][j]
+                columns.append(number)
+    
+            rowss = []
+            for i in range(len(self.matrix)):
+                new_number = 0
+                for k in range(len(self.matrix[0])):
+                    new_number += self.matrix[i][k] * columns[k]
+                rowss.append(new_number)
+            matrix_m.append(rowss)
+        for l in matrix_m:
+            print(l)
+
+
+       
+            
+        
+        
+            
+    
+    
+    
+        
 
     def print_matrix(self):
         for row in self.matrix:
@@ -62,6 +80,8 @@ def main():
     matrix_instance = matrix()
     matrix_instance.create_matrix()
     matrix_instance.print_matrix()
+    matrix_instance.display_row()
+    matrix_instance.display_column()
     matrix_instance.multiplication()
 
 
